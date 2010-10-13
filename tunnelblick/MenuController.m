berkus/tunnelblick

commit 04a96a102fd58c3e08596452d88430741900d24c

Build 2068: Adds workaround for bug in OS X 10.4.11 ('Tiger') that causes misdraws of the 'Set nameserver' button and the 'Monitor connection' checkbox.
git-svn-id: http://tunnelblick.googlecode.com/svn/trunk@1068 ecaf4b9d-c145-0410-a236-1b700048ef5c

tunnelblick/Info.plist
@@ -34,11 +34,11 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>3.1beta17 (build 2062)</string>
+	<string>3.1beta17 (build 2068)</string>
 	<key>CFBundleSignature</key>
 	<string>OVPN</string>
 	<key>CFBundleVersion</key>
-	<string>2062</string>
+	<string>2068</string>
 	<key>NSMainNibFile</key>
 	<string>MainMenu</string>
 	<key>NSPrincipalClass</key>

tunnelblick/MenuController.m
@@ -1733,6 +1733,9 @@ extern BOOL checkOwnerAndPermissions(NSString * fPath, uid_t uid, gid_t gid, NSS
     }
     // ***** End duplication of the effect of [self setSelectedModifyNameserverIndex: index] but without calling ourselves
     
+    [modifyNameserverPopUpButton setNeedsDisplay]; // Workaround for bug in OS X 10.4.11 ("Tiger") that causes misdraws
+    [monitorConnnectionCheckbox  setNeedsDisplay];
+    
     if (  [gTbDefaults canChangeValueForKey: useDNSKey]  ) {
         [modifyNameserverPopUpButton setEnabled: YES];
     } else {

commit 3c6a4299dac8a6c5d336ff0b7f98c3d11ff8a5b5

Build 1070: Fixes problem introduced in r1068 that causes 'Monitor connection' to not be cleared when a connection is changed from anything to 'Do not set nameserver'

tunnelblick/Info.plist
@@ -34,11 +34,11 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>3.1beta17 (build 2069)</string>
+	<string>3.1beta17 (build 2070)</string>
 	<key>CFBundleSignature</key>
 	<string>OVPN</string>
 	<key>CFBundleVersion</key>
-	<string>2069</string>
+	<string>2070</string>
 	<key>NSMainNibFile</key>
 	<string>MainMenu</string>
 	<key>NSPrincipalClass</key>

tunnelblick/MenuController.m
@@ -202,6 +202,8 @@ extern BOOL checkOwnerAndPermissions(NSString * fPath, uid_t uid, gid_t gid, NSS
         tunCount = 0;
         tapCount = 0;
         
+        selectedModifyNameserverIndex = -1; // Won't match any valid value, so when first set it will trigger a change notice
+        
         gFileMgr    = [NSFileManager defaultManager];
         
         gDeployPath = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent: @"Deploy"] copy];